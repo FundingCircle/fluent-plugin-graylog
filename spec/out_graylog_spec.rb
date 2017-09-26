7914125689f4aa6a988c43852ff553f6647ceb8f
Timothy Stott <stott.timothy@gmail.com>
2017-09-26 17:19:50 +0100

    :cop: Fix Rubocop offences
    Post bot upgrade fixes

--- spec/out_graylog_spec.rb
+++ spec/out_graylog_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe Fluent::GrayLogOutput do
+RSpec.describe Fluent::GrayLogOutput do
   let(:driver) do
     Fluent::Test.setup
     Fluent::Test::OutputTestDriver.new(Fluent::GrayLogOutput).configure(config)
